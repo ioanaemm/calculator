442952056ae0cedfc458afb9a10cd6faac403c3b
ioanaemm <miteleaioana@gmail.com>
2018-11-22 21:55:57 +0000

    Start adding unit tests

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,6 +14,7 @@ class App extends Component {
     this.onSymbolClick = this.onSymbolClick.bind(this);
     this.onNumberClick = this.onNumberClick.bind(this);
     this.onEqualClick = this.onEqualClick.bind(this);
+    this.displayNumbers = this.displayNumbers.bind(this);
   }
 
   onSymbolClick(symbol) {
@@ -23,37 +24,51 @@ class App extends Component {
   onNumberClick(number) {
     if (this.state.symbol === "") {
       if (this.state.firstTerm === "0") {
-        this.setState({ firstTerm: number });
+        this.setState({ firstTerm: String(number) });
       } else {
-        this.setState({ firstTerm: this.state.firstTerm + number });
+        this.setState({ firstTerm: this.state.firstTerm + String(number) });
       }
     } else {
-      this.setState({ secondTerm: this.state.secondTerm + number });
+      if (this.state.secondTerm === "0") {
+        this.setState({ secondTerm: String(number) });
+      } else {
+        this.setState({ secondTerm: this.state.secondTerm + String(number) });
+      }
     }
   }
 
   onEqualClick() {
-    if (this.state.symbol == "+") {
+    if (this.state.symbol === "+") {
       this.setState({
         firstTerm: String(
           parseInt(this.state.firstTerm) + parseInt(this.state.secondTerm)
         ),
-        secondTerm: ""
+        secondTerm: "0",
+        symbol: ""
       });
-    } else if (this.state.symbol == "-") {
+    } else if (this.state.symbol === "-") {
       this.setState({
         firstTerm: String(
           parseInt(this.state.firstTerm) - parseInt(this.state.secondTerm)
         ),
-        secondTerm: ""
+        secondTerm: "0",
+        symbol: ""
       });
+    }
+  }
+
+  displayNumbers() {
+    if (this.state.symbol === "") {
+      return this.state.firstTerm;
+    } else {
+      return this.state.secondTerm;
     }
   }
 
   render() {
     return (
       <div className="App">
-        <Header result={this.state.firstTerm} />
+        <Header result={this.displayNumbers()} />
         <Body
           onSymbolClick={this.onSymbolClick}
           onNumberClick={this.onNumberClick}
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Renderer from "react-test-renderer";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+jest.mock("../Header/Header", () => "Header");
+jest.mock("../Body/Body", () => "Body");
+
+describe("App component", () => {
+  it("renders without crashing", () => {
+    const component = Renderer.create(<App />);
+    expect(component).toBeDefined();
+  });
+
+  it("renders correctly", () => {
+    const component = Renderer.create(<App />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it("sets the symbol", () => {
+    const component = Renderer.create(<App />);
+    const instance = component.getInstance();
+    instance.onSymbolClick("foo");
+    expect(instance.state.symbol).toEqual("foo");
+  });
+
+  it("handles number when the first term is 0 and when no symbol", () => {
+    const component = Renderer.create(<App />);
+    const instance = component.getInstance();
+    instance.state = {
+      symbol: "",
+      firstTerm: "0"
+    };
+    instance.onNumberClick("3");
+    expect(instance.state.firstTerm).toEqual("3");
+  });
+
+  it("handles number when the first term is not 0 and no symbol", () => {
+    const component = Renderer.create(<App />);
+    const instance = component.getInstance();
+    instance.state = {
+      symbol: "",
+      firstTerm: "69"
+    };
+    instance.onNumberClick("6");
+    expect(instance.state.firstTerm).toEqual("696");
+  });
+
+  it("handles number when the second term is 0 and a symbol", () => {
+    // setup
+    const component = Renderer.create(<App />);
+    const instance = component.getInstance();
+
+    // setting the stage
+    instance.state = {
+      symbol: "+",
+      firstTerm: "345",
+      secondTerm: "0"
+    };
+
+    // mocks
+    const mockSetState = jest.fn();
+    instance.setState = mockSetState;
+
+    // code to be tested
+    instance.onNumberClick("5");
+
+    // assertions
+    expect(mockSetState.mock.calls.length).toEqual(1);
+    expect(mockSetState).toBeCalledWith({ secondTerm: "5" });
+  });
+
+  it("handles number when there is a symbol", () => {
+    const component = Renderer.create(<App />);
+    const instance = component.getInstance();
+    instance.state = {
+      symbol: "+",
+      secondTerm: "7"
+    };
+    instance.onNumberClick("9");
+    expect(instance.state.secondTerm).toEqual("79");
+  });
 });
